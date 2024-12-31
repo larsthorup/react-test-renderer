a7bdef5be6d64ec262785399e343223548e1a519
Lars Thorup <lars@zealake.com>
2024-12-31 08:49:31 +0100

    instance.ts - types

--- src/instance.ts
+++ src/instance.ts
@@ -14,65 +14,61 @@ export function isInstance(node: Node): node is Instance {
   return 'type' in node;
 }
 
-type NodePredicate = (node: Node) => boolean;
+type Predicate = (instance: Instance) => boolean;
 
 export function find(
-  node: Node,
-  predicate: NodePredicate,
+  instance: Instance,
+  predicate: Predicate,
 ): Instance | undefined {
-  if (!isInstance(node)) return undefined;
-  if (predicate(node)) {
-    return node;
+  if (predicate(instance)) {
+    return instance;
   }
-  for (const child of node.children) {
-    const found = find(child, predicate);
-    if (found) {
-      return found;
+  for (const child of instance.children) {
+    if (isInstance(child)) {
+      const found = find(child, predicate);
+      if (found) {
+        return found;
+      }
     }
   }
 }
 
-export function findAll(node: Node, predicate: NodePredicate): Instance[] {
+export function findAll(instance: Instance, predicate: Predicate): Instance[] {
   const found = [];
-  if (isInstance(node)) {
-    if (predicate(node)) {
-      found.push(node);
-    }
-    for (const child of node.children) {
+  if (predicate(instance)) {
+    found.push(instance);
+  }
+  for (const child of instance.children) {
+    if (isInstance(child)) {
       found.push(...findAll(child, predicate));
     }
   }
   return found;
 }
 
-export const byType = (type: string) => (node: Node) =>
-  isInstance(node) && node.type === type;
+export const byType = (type: string) => (instance: Instance) =>
+  instance.type === type;
 
-export const byText = (text: string) => (node: Node) =>
-  isInstance(node) &&
-  node.children[0] !== undefined &&
-  !isInstance(node.children[0]) &&
-  node.children[0].text === text;
+export const byText = (text: string) => (instance: Instance) =>
+  instance.children[0] !== undefined &&
+  !isInstance(instance.children[0]) &&
+  instance.children[0].text === text;
 
-export const byProps = (props: Instance['props']) => (node: Node) => {
-  if (isInstance(node)) {
-    for (const key in props) {
-      if (
-        typeof node.props[key] === 'object' &&
-        typeof props[key] === 'object' &&
-        node.props[key] !== null &&
-        props[key] !== null
-      ) {
-        const value = node.props[key] as Record<string, object>;
-        return Object.entries(props[key]).every(
-          ([subKey, subValue]) => value[subKey] === subValue,
-        );
-      } else if (node.props[key] !== props[key]) {
-        return false;
-      }
+export const byProps = (props: Instance['props']) => (instance: Instance) => {
+  for (const key in props) {
+    if (
+      typeof instance.props[key] === 'object' &&
+      typeof props[key] === 'object' &&
+      instance.props[key] !== null &&
+      props[key] !== null
+    ) {
+      const value = instance.props[key] as Record<string, object>;
+      return Object.entries(props[key]).every(
+        ([subKey, subValue]) => value[subKey] === subValue,
+      );
+    } else if (instance.props[key] !== props[key]) {
+      return false;
     }
-    return true;
-  } else {
-    return false;
   }
+  return true;
 };
