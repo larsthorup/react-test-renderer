11fe70fbb55170ac2b1c35e8fc5107f75b9211fe
Lars Thorup <lars@zealake.com>
2024-12-31 08:42:26 +0100

    instance.ts

--- src/instance.test.ts
+++ src/instance.test.ts
@@ -1,12 +1,134 @@
-import { test, expect } from 'vitest';
-import { isInstance, type Instance, type Node } from './instance.js';
+import { test, expect, describe, assertType } from 'vitest';
+import type { Instance, Node, TextInstance } from './instance.js';
+import {
+  byProps,
+  byText,
+  byType,
+  find,
+  findAll,
+  isInstance,
+} from './instance.js';
 
-test(isInstance.name, () => {
-  const instance: Node = { type: 'div', props: {}, children: [] };
-  expect(isInstance(instance)).toBe(true);
-  expect(instance.type).toBe('div');
+const leafText = { text: 'Hello, world!' };
+const leaf = { type: 'b', props: {}, children: [leafText] };
+const child1 = {
+  type: 'span',
+  props: { className: 'primary', style: { color: 'red', padding: '2px' } },
+  children: [leaf],
+};
+const child2 = {
+  type: 'span',
+  props: {
+    className: 'secondary',
+    ariaLabel: 'Hello, world!',
+    style: { color: 'blue' },
+  },
+  children: [{ text: 'Hello, world!' }],
+};
+const root: Node = {
+  type: 'div',
+  props: { onClick: () => {} },
+  children: [child1, child2],
+};
 
-  const textInstance: Node = { text: 'Hello, world!' };
-  expect(isInstance(textInstance)).toBe(false);
-  expect(textInstance.text).toBe('Hello, world!');
+describe(isInstance.name, () => {
+  test('Instance', () => {
+    const instance: Node = { type: 'div', props: {}, children: [] };
+    expect(isInstance(instance)).toBe(true);
+    assertType<Instance>(instance);
+    expect(instance.type).toBe('div');
+  });
+
+  test('TextInstance', () => {
+    const textInstance: Node = { text: 'Hello, world!' };
+    expect(isInstance(textInstance)).toBe(false);
+    assertType<TextInstance>(textInstance);
+    expect(textInstance.text).toBe('Hello, world!');
+  });
+});
+
+describe(find.name, () => {
+  test('root', () => {
+    const instance = find(root, byType('div'));
+    expect(instance).toBe(root);
+  });
+  test('child', () => {
+    const instance = find(root, byType('span'));
+    expect(instance).toBe(child1);
+  });
+  test('leaf', () => {
+    const instance = find(root, byType('b'));
+    expect(instance).toBe(leaf);
+  });
+  test('first', () => {
+    const instance = find(root, byType('span'));
+    expect(instance).toBe(child1);
+  });
+});
+
+describe(findAll.name, () => {
+  test('none', () => {
+    const instances = findAll(root, byType('a'));
+    expect(instances).toEqual([]);
+  });
+
+  test('one', () => {
+    const instances = findAll(root, byType('b'));
+    expect(instances).toEqual([leaf]);
+  });
+
+  test('all', () => {
+    const instances = findAll(root, byType('span'));
+    expect(instances).toEqual([child1, child2]);
+  });
+});
+
+describe(byText.name, () => {
+  test('found', () => {
+    const instance = find(root, byText('Hello, world!'));
+    expect(instance).toBe(leaf);
+  });
+
+  test('not found', () => {
+    const instance = find(root, byText('Goodbye, world!'));
+    expect(instance).toBe(undefined);
+  });
+});
+
+describe(byProps.name, () => {
+  test('subset of props', () => {
+    const instance = find(root, byProps({ className: 'primary' }));
+    expect(instance).toBe(child1);
+  });
+
+  test('all props', () => {
+    const instance = find(
+      root,
+      byProps({ className: 'secondary', ariaLabel: 'Hello, world!' }),
+    );
+    expect(instance).toBe(child2);
+  });
+
+  test('missing props', () => {
+    const instance = find(root, byProps({ className: 'tertiary' }));
+    expect(instance).toBe(undefined);
+  });
+
+  test('subset of sub props', () => {
+    const instance = find(root, byProps({ style: { color: 'blue' } }));
+    expect(instance).toBe(child2);
+  });
+
+  test('all sub props', () => {
+    const instance = find(
+      root,
+      byProps({ style: { color: 'red', padding: '2px' } }),
+    );
+    expect(instance).toBe(child1);
+  });
+
+  test('missing sub props', () => {
+    const instance = find(root, byProps({ style: { color: 'orange' } }));
+    expect(instance).toBe(undefined);
+  });
 });
--- src/instance.ts
+++ src/instance.ts
@@ -13,3 +13,66 @@ export type Node = Instance | TextInstance;
 export function isInstance(node: Node): node is Instance {
   return 'type' in node;
 }
+
+type NodePredicate = (node: Node) => boolean;
+
+export function find(
+  node: Node,
+  predicate: NodePredicate,
+): Instance | undefined {
+  if (!isInstance(node)) return undefined;
+  if (predicate(node)) {
+    return node;
+  }
+  for (const child of node.children) {
+    const found = find(child, predicate);
+    if (found) {
+      return found;
+    }
+  }
+}
+
+export function findAll(node: Node, predicate: NodePredicate): Instance[] {
+  const found = [];
+  if (isInstance(node)) {
+    if (predicate(node)) {
+      found.push(node);
+    }
+    for (const child of node.children) {
+      found.push(...findAll(child, predicate));
+    }
+  }
+  return found;
+}
+
+export const byType = (type: string) => (node: Node) =>
+  isInstance(node) && node.type === type;
+
+export const byText = (text: string) => (node: Node) =>
+  isInstance(node) &&
+  node.children[0] !== undefined &&
+  !isInstance(node.children[0]) &&
+  node.children[0].text === text;
+
+export const byProps = (props: Instance['props']) => (node: Node) => {
+  if (isInstance(node)) {
+    for (const key in props) {
+      if (
+        typeof node.props[key] === 'object' &&
+        typeof props[key] === 'object' &&
+        node.props[key] !== null &&
+        props[key] !== null
+      ) {
+        const value = node.props[key] as Record<string, object>;
+        return Object.entries(props[key]).every(
+          ([subKey, subValue]) => value[subKey] === subValue,
+        );
+      } else if (node.props[key] !== props[key]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
